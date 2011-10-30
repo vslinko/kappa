72b39c8a658919ed01cc761ad5626ea9ca4d9418
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2011-10-30 13:02:50 +0400

    Fetching necessary information

--- app/app.php
+++ app/app.php
@@ -15,9 +15,50 @@ foreach ($config as $key => $value) {
 
 $app->register(new Kappa\KayakoProvider());
 
-$app->get('/', function () {
-    $staff = kyStaff::getAll();
-    $text = print_r($staff, true);
+$storage = function($key, $factory, $ttl = 3600) {
+    $success = false;
+    $result = apc_fetch($key, $success);
+
+    if (!$success) {
+        $result = $factory();
+
+        apc_store($key, $result, $ttl);
+    }
+
+    return $result;
+};
+
+$app->get('/', function () use ($app, $storage) {
+    $department = $storage('kappa:department', function () use ($app) {
+        return kyDepartment::get($app['kappa.department']);
+    });
+
+    $statuses = $storage('kappa:statuses', function () use ($app) {
+        $statuses = array();
+
+        foreach ($app['kappa.statuses'] as $statusId) {
+            $statuses[] = kyTicketStatus::get($statusId);
+        }
+
+        return new kyResultSet($statuses);
+    });
+
+    $staffs = $storage('kappa:staff', function () use ($app) {
+        $staffs = array();
+
+        foreach ($app['kappa.staff'] as $staffId) {
+            $staffs[] = kyStaff::get($staffId);
+        }
+
+        return new kyResultSet($staffs);
+    });
+
+    $tickets = kyTicket::getAll($department, $statuses, $staffs);
+
+    $text = print_r($department, true);
+    $text .= print_r($statuses, true);
+    $text .= print_r($staffs, true);
+    $text .= print_r($tickets, true);
 
     return new Symfony\Component\HttpFoundation\Response($text, 200, array('Content-Type' => 'text/plain'));
 });
--- app/config.example.php
+++ app/config.example.php
@@ -5,4 +5,8 @@ return array(
     'kayako.api_key' => '',
     'kayako.secret_key' => '',
     'kayako.class_path' => __DIR__ . '/../vendor/kayako',
+
+    'kappa.department' => 0,
+    'kappa.statuses' => array(),
+    'kappa.staff' => array(),
 );
